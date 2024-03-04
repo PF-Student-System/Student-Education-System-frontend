3c17fd4748401d72e98319227d337d83f92aa1ea
Mugheessaad <kambohsaad@gmail.com>
2024-03-04 17:56:51 +0500

    made page of ExamStudent and its mcqs component

---
+++ components/McqCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mcqcard bg-green-100">
+      <p class="text-xl font-bold " style="color: #535966;">{{ questions.questionText }}</p>
+      <ul>
+        <li v-for="(option, optionIndex) in questions.options" :key="optionIndex" class="mt-2">
+          <input type="radio" :id="`question_${index}`" :name="`question_${index}`" class="mr-2">
+          <label :for="`option_${optionIndex}`" class="mt-2">{{ option }}</label>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script setup>
+    const { questions } = defineProps(['questions'])
+    console.log(questions)
+</script>
+<style scoped>
+input[type="radio"] {
+  appearance: none;
+  width: 16px;
+  height: 16px;
+  border: 2px solid #333; /* Border color */
+  border-radius: 50%; /* Rounded shape */
+  outline: none;
+  margin-right: 8px;
+}
+    input[type="radio"]:checked {
+  background-color: #00C16A; /* Background color when checked */
+    }
+</style>
---
+++ pages/ExamStudent/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-for="questions in myExam.questions" :key="questions.questionText" class="card">
+          <mcqCard :questions="questions"/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { myExam } from '~/types/exam';
+      onMounted(() => {
+            console.log("hello" ,myExam)
+        }) 
+
+</script>
+
+<style scoped>
+
+</style>
