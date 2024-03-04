38930a69707ec8657d28b9aa9e3868fcb6fa3603
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-03-04 15:37:00 +0500

    added custom tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+    colors: {
+      customgreen: "#00C16A",
+      customblue: "#0F172A",
+      customgraytext: "#B2BAC6",
+      customgrayheading: "#535966",
+    },
+  },
+  plugins: [],
+};
