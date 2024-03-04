8dee41e2ccba2f8125a58dbfca56d3ee55288585
amjad <kamjad78661@gmaial.com>
2024-03-04 18:22:45 +0500

    signup page created

---
+++ pages/Signup.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <h1>SignUp</h1>
+    <h3>Step 1 out of 2</h3>
+    <input type="text" placeholder="First Name" >
+
+    <input type="text" placeholder="Last Name">
+
+    <button class="btn">Next</button>
+
+<slot />
+</template>
+<script>
+</script>
