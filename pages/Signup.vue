d0170f8cc919b1cf6bed89c654a8b9b9b77d5e9a
amjad <kamjad78661@gmaial.com>
2024-03-05 16:42:56 +0500

    capture function added in singin/index page

--- pages/Signup.vue
+++ pages/Signup.vue
@@ -1,14 +1,20 @@
 <template>
-
-    <h1>SignUp</h1>
-    <h3>Step 1 out of 2</h3>
-    <input type="text" placeholder="First Name" >
-
-    <input type="text" placeholder="Last Name">
-
-    <button class="btn">Next</button>
-
-<slot />
+    <dev class="text-center mt-36 font-bold text-2xl">
+        <h1>SignUp</h1>
+        <h3>Step 1 out of 2</h3>
+        <dev style="margin: auto; padding: auto;">
+            <input type="text" v-model="firstname" placeholder="First Name" style="border: 1px solid black; margin-left: 300px; padding: 6px; border-radius: 5px;">
+        </dev>
+        <br>  <br>
+        <dev>
+            <input type="text" v-model="lastname" placeholder="Last Name" style="border: 1px solid black; margin-left: 300px; padding: 6px; border-radius: 5px;">
+        </dev>
+        <br> <br>
+        <button style="margin-left: 400px;" class="btn">Next</button>
+    </dev>
 </template>
 <script>
+
+
+
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="text-center mt-36 font-bold text-2xl">
-    <h1>Sign in</h1>
+    <h1>Sign In</h1>
+    <div style="border: 2px solid black; width:400px; height: 400px;">
+      <video ref="player" controls autoplay></video>
+    </div>
+    <div>
+      <button class="btn" @click="captureImage">Capture</button>
+    </div>
+    <canvas ref="canvas" width="500" height="400"></canvas>
+  </div>
+  <div>
+    <button class="btn">Sign in</button>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const player = ref(null);
+const canvas = ref(null);
+
+const constraints = {
+  video: {
+    width: 1280,
+    height: 720
+  }
+};
+
+async function initCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    if (player.value) {
+      player.value.srcObject = stream;
+    }
+  } catch (e) {
+    console.error(`navigator.getUserMedia error: ${e.toString()}`);
+  }
+}
+
+function captureImage() {
+  if (player.value && canvas.value) {
+    const context = canvas.value.getContext('2d');
+    context.drawImage(player.value, 0, 0, canvas.value.width, canvas.value.height);
+    player.srcObject.getVideoTracks().forEach(track => track.stop());
+  }
+}
+
+onMounted(() => {
+  initCamera();
+});
+</script>
