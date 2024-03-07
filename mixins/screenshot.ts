f6a9182979bcc63e0c1d1b3d3b1f87cff55a00f7
Mugheessaad <kambohsaad@gmail.com>
2024-03-07 14:37:17 +0500

    Completed Exam modeule with screen shot module fully functional and made it with typescript

---
+++ mixins/screenshot.ts
@@ -0,0 +1,34 @@
+import html2canvas from "html2canvas";
+
+export default function screenshot () {
+    let frames: string[] = [];
+    let intervalId: ReturnType<typeof setInterval>;
+
+    async function takeScreenshot() {
+        intervalId = setInterval(takeScreenshotOnLoop, 4000);
+    }  
+    async function takeScreenshotOnLoop() {
+        const { scrollX, scrollY, innerWidth, innerHeight } = window;
+        const visibleArea = {
+            x: scrollX,
+            y: scrollY,
+            width: innerWidth,
+            height: innerHeight
+        };
+    
+        const canvas = await html2canvas(document.documentElement, visibleArea);
+        const imageUrl = canvas.toDataURL();
+        frames.push(imageUrl);
+        console.log('Screenshot taken', frames);
+    }
+    
+    function stopCapture() {
+        clearInterval(intervalId);
+    }
+
+    return {
+        takeScreenshot,
+        stopCapture,
+        frames,
+    };
+}
