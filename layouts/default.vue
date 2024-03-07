2ac475e486a0cb28b29a3d2ef1fdbbfa217a260a
Hafiz Muhammad Ahtsham <hafizmuhammadahtsham@gmail.com>
2024-03-07 16:07:31 +0500

    footer completed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
-<template>   
-  <AppHeader />
-  <slot />
-  <AppFooter />
+<template>
+  <div>
+    <head>
+      <!-- Other meta tags and stylesheets -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
 </template>
+
+<script>
+export default {
+  // Your script logic here
+}
+</script>
+
+<style>
+/* Your styles here */
+</style>
