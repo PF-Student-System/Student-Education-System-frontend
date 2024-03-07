d9f6822460903209ab7d12fe05bae6bac3e999e9
Ali Hussain Khan <79786177+Alihussain-khan@users.noreply.github.com>
2024-03-07 17:31:52 +0500

    Delete .output directory

--- .output/server/chunks/build/dashboardSidebar-styles.BooFomYs.mjs
+++
@@ -1,6 +0,0 @@
-const dashboardSidebar_vue_vue_type_style_index_0_lang = ".customwidth{width:90%}.customwidth:hover{background:#f0efef}";
-
-const dashboardSidebarStyles_BooFomYs = [dashboardSidebar_vue_vue_type_style_index_0_lang];
-
-export { dashboardSidebarStyles_BooFomYs as default };
-//# sourceMappingURL=dashboardSidebar-styles.BooFomYs.mjs.map
