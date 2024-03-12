828b7c297e0f3b3dd9204139c5f127432314efe4
Nouman-blip <attaussmalikki@gmail.com>
2024-03-12 10:54:53 +0500

    pages content changes

--- pages/Lectures/index.vue
+++ pages/Lectures/index.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="max-w-2xl mx-auto mt-8">
-    <video controls class="w-full shadow-lg rounded-lg" @error="handleVideoError">
-      <source :src="videoUrl" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+  <div>
+    <handlingLectures @lectureFinished="handleLectureFinished" />
     <nuxt-link to="/lectureSuccessful" class="no-underline">
-      <button class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">
+      <button :disabled="!finishEnabled" class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">
         Finish Lecture
       </button>
     </nuxt-link>
-
-    <div>
-      <button @click="takeScreenshot">Take screenshot</button>
-      <button @click="stopCapture">Stop capture</button>
-      <button @click="startSlideshow(frames)">Start slideshow</button>
-      <img :src="currentImage" alt="Current Image" v-if="currentImage">
+    <!-- Display the slideshow buttons and image -->
+    <div class="m-4">
+      <button @click="startSlideshow(lectureData.images)">Start Images Slideshow</button>
+      <button @click="startSlideshow(lectureData.frames)">Start Frames Slideshow</button>
+      <img :src="currentImage" alt="currentImages" v-if="currentImage">
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import screenshot from '@/mixins/screenshot';
+import { ref,reactive } from 'vue';
 import { slideShow } from '@/mixins/VideoShow';
 
-
-const videoUrl = ref(new URL('@/assets/mov_bbb.mp4', import.meta.url).href);
-const { takeScreenshot, stopCapture, frames } = screenshot();
+const finishEnabled = ref(false);
+const lectureData = reactive({ images: [], frames: [] });
 const { startSlideshow, currentImage } = slideShow();
 
-onMounted(() => {
-  videoUrl.value = new URL('@/assets/mov_bbb.mp4', import.meta.url).href;
-});
-
-const handleVideoError = (event) => {
-  console.error("Error loading video:", event);
+const handleLectureFinished = (data) => {
+  finishEnabled.value = true;
+  lectureData.images = data.imagearray;
+  lectureData.frames = data.frames;
+  console.log("Images:", lectureData.images);
+  console.log("Frames:", lectureData.frames);
 };
 
 </script>
